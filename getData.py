98bd096589e9aaec8b077503b454da533ceb633b
nikmedoed <grafniksan@gmail.com>
2017-11-23 09:19:02 +0300

    фвв phantom

--- getData.py
+++ getData.py
@@ -4,15 +4,38 @@ import requests
 from lxml import html
 
 from selenium import webdriver
-
+from time import sleep
 
 def getData():
+
+    last = '2088-427591'
     link = "http://informatics.mccme.ru/moodle/submits/view.php?group_id=8137#1"
     browser = webdriver.Chrome()
     browser.get(link)
-    assert "Личные " in browser.title
-    # elem = browser.find_element_by_name("tbody")
-    # e = browser.
+    sleep(3)
+
+    names = set()
+    new = []
+
+    elem = browser.find_elements_by_xpath('//*[@id="Searchresult"]/table/tbody/tr')
+    del(elem[0])
+    start = elem[1].text.split()[0]
+    print("Найдено элементов",len(elem))
+    for el in elem:
+        e= el.find_elements_by_tag_name('td')
+        name = e[1].text
+        tlast = e[0].text
+        task = e[2].text.split(".")[0]
+        score = round(int(e[7].text)/100,2)
+        names.add(name)
+
+
+        print(len(e))
+    #     te = elem[1].text
+    # print(elem[1].text)
+
+
+
     browser.close()
     # requests.packages.urllib3.disable_warnings()
     # u = requests.api.request('post', link, data={'bar': 'baz'}, json=None, verify=False).text
