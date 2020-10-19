cb7ed22a820a238386d3fa7a2523c7c846461950
Nikita <grafniksan@gmail.com>
2020-10-19 15:17:29 +0300

    new design update (09.2020)

--- app/browser.py
+++ app/browser.py
@@ -3,9 +3,7 @@ import os
 
 def getBrowser():
     temp = os.getcwd()
-    # os.chdir(temp+'\\app')
     os.chdir('app')
-    # print(os.getcwd())
     browser = webdriver.Chrome()
     os.chdir(temp)
     return browser
